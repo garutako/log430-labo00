c204a5fee116078e9ee2cfe0aff4182cd437fb4f
garutako <adam-hamid.salah-salah.1@ens.etsmtl.ca>
2026-09-16 00:05:44 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -6,9 +6,28 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
+my_calculator = Calculator()
+
 def test_app():
-    my_calculator = Calculator()
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
-# TODO: ajoutez les tests
+
+def test_addition():
+    assert my_calculator.addition(10, 2) == 12
+
+
+def test_soustraction():
+    assert my_calculator.subtraction(10, 2) == 8
+
+
+def test_multiplication():
+    assert my_calculator.multiplication(10, 2) == 20
+
+
+def test_division():
+    assert my_calculator.division(10, 2) == 5
+
+
+def test_division_par_0():
+    assert my_calculator.division(10, 0) == "Erreur : division par zéro"
